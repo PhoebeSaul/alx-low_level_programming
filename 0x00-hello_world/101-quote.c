faca4d12c755eb417a7bfb3b623e27103eb5dfe2
PhoebeSaul <sanyuphoebe@gmail.com>
2023-03-16 05:58:05 -0700

    Quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unsistd.h>
+/**
+* main - A C program that prints a line to the standard error
+* Return: 1 (Sucess)
+*/
+int main(void)
+{
+fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout); return (1);
+}
